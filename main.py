c2acf198f82c4213ff0ec7de0fceb84afda16097
Diogo Carapito <43778648+DiogoCarapito@users.noreply.github.com>
2023-10-25 20:41:45 +0100

    main.py updates

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from utils import utils
 
@@ -12,3 +13,7 @@ async def root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, host="0.0.0.0")
