d6793074fae91257bb0ea59b219ad04572112893
Diogo Carapito <43778648+DiogoCarapito@users.noreply.github.com>
2023-10-25 20:12:02 +0100

    new test_main.py code

--- test_main.py
+++ test_main.py
@@ -1,47 +1,14 @@
-# lest build a test main.py with pytest
+# test with pytest for main.py
 
 import pytest
 from fastapi.testclient import TestClient
 from main import app
 
+client = TestClient(app)
 
-# Web Application Testing
-@pytest.fixture
-def client():
-    with TestClient(app) as client:
-        yield client
-
-
-# Test the root endpoint
-def test_main_read(client):
+# test root
+def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
 
-
-# Test the items endpoint
-def test_read_item(client):
-    response = client.get("/items/1")
-    assert response.status_code == 200
-    assert response.json() == {"item_id": 1}
-
-
-"""
-import pytest
-from fastapi.testclient import TestClient
-from main import app
-
-
-#### Web Application Testing
-@pytest.fixture
-def client():
-    with TestClient(app) as client:
-        yield client
-
-
-def test_main_read(client):
-    response = client.get("/")
-    assert response.status_code == 200
-    # assert response.json() == {"Hello": "Hello world!"}
-
-"""
