01caaa753efd12928bb5e096cc43a813db8ef109
Diogo Carapito <43778648+DiogoCarapito@users.noreply.github.com>
2023-10-25 20:31:51 +0100

    lint errors corrected

--- main.py
+++ main.py
@@ -12,4 +12,3 @@ async def root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
-
--- test_main.py
+++ test_main.py
@@ -1,11 +1,3 @@
-"""
-this is a test file for main.py
-using pytest
-it will test root path and items path
-it should check if the response is 200
-"""
-
-import pytest
 from fastapi.testclient import TestClient
 from main import app
 
@@ -17,8 +9,8 @@ def test_root():
     assert response.status_code == 200
     assert response.json() == {"message": "hello world!"}
 
+
 def test_items():
     response = client.get("/items/1")
     assert response.status_code == 200
     assert response.json() == {"item_id": 1}
-
